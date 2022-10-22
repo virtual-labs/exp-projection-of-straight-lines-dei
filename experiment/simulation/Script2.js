4417f331a2d60dd29bc381e5c4756cc9d0bf39fd
Prateek Dubey <112685450+ishantdei@users.noreply.github.com>
2022-10-22 12:03:34 +0530

    Updated

--- experiment/simulation/Script2.js
+++ experiment/simulation/Script2.js
@@ -5,6 +5,7 @@ function getValue(option) {
   if (option.value =="pen")
    {debugger;
     document.getElementById("png4").style.visibility = "visible";
+    document.getElementById("png5").style.visibility = "visible";
     document.getElementById("numoutnext").innerHTML = "";
   }
 
@@ -12,6 +13,15 @@ function getValue(option) {
    {
     document.getElementById("numoutnext").innerHTML="Select 3H pencil";
     document.getElementById("png4").style.visibility = "hidden";
+    document.getElementById("png5").style.visibility = "hidden";
     
   }
+}
+
+function movepen(){
+  document.getElementById("png6").style.visibility="visible";
+  document.getElementById("png").style.visibility="hidden";
+  document.getElementById("png5").style.visibility="hidden";
+  document.getElementById("png4").style.visibility="hidden";
+
 }
