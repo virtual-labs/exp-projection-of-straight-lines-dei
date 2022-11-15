ee493329710504dbe55d7c2683ad34f0c85e0394
vleadadmin <admin@vlabs.ac.in>
2022-11-15 16:33:11 +0000

    https://virtual-labs.github.io/virtual-labs/exp-projection-of-straight-lines-dei click on the link to test your code.

---
+++ simulation/Script3.js
@@ -0,0 +1,111 @@
+
+let greeting;
+function getValue(option) {
+
+  if (option.value =="pen")
+   {
+    document.getElementById("png4").style.visibility = "visible";
+    document.getElementById("png5").style.visibility = "visible";
+    document.getElementById("numoutnext").innerHTML = "";
+  }
+
+  else
+   {
+    document.getElementById("numoutnext").innerHTML="Select 3H pencil";
+    document.getElementById("png4").style.visibility = "hidden";
+    document.getElementById("png5").style.visibility = "hidden";
+    
+  }
+}
+
+function getValue1(option) {
+
+  if (option.value =="penn")
+   {
+    document.getElementById("png3").style.visibility = "visible";
+    document.getElementById("png5").style.visibility = "visible";
+    document.getElementById("numoutnext").innerHTML = "";
+  }
+
+  else
+   {
+    document.getElementById("numoutnext").innerHTML="Select 2H pencil";
+    document.getElementById("png3").style.visibility = "hidden";
+    document.getElementById("png5").style.visibility = "hidden";
+    
+  }
+}
+
+function movepen(){
+  document.getElementById("png6").style.visibility="visible";
+  document.getElementById("png").style.visibility="hidden";
+  document.getElementById("png5").style.visibility="hidden";
+  document.getElementById("dr").style.visibility="hidden";
+  document.getElementById("png4").style.visibility="hidden";
+  myTimeout = setTimeout(myGreeting, 9000);
+
+}
+
+function nextst(){
+  document.getElementById("png8").style.visibility="visible";
+  document.getElementById("png6").style.visibility="hidden";
+  document.getElementById("png5").style.visibility="hidden";
+  document.getElementById("dr1").style.visibility="hidden";
+  document.getElementById("png3").style.visibility="hidden";
+  myTimeout1 = setTimeout(myGreeting1, 15000);
+
+}
+
+function nextst1(){
+  document.getElementById("png9").style.visibility="visible";
+  document.getElementById("png10").style.visibility="hidden";
+  document.getElementById("png5").style.visibility="hidden";
+  document.getElementById("dr1").style.visibility="hidden";
+  myTimeout2 = setTimeout(myGreeting2, 10000);
+
+}
+
+
+function myGreeting()
+     {
+    // document.getElementById("png5").style.visibility="visible";
+    document.getElementById("dr1").style.visibility="visible";
+    document.getElementById("canvas2").style.visibility="visible";
+    document.getElementById("canvas1").style.visibility="hidden";
+    
+    myStopFunction();
+  }
+  
+  function myStopFunction() {
+    clearTimeout(myTimeout);
+
+  
+}
+
+function myGreeting1()
+     {
+    document.getElementById("png5").style.visibility="visible";
+    document.getElementById("png10").style.visibility="visible";
+    document.getElementById("canvas3").style.visibility="visible";
+    document.getElementById("canvas2").style.visibility="hidden";
+    myStopFunction1();
+  }
+  
+  function myStopFunction1() {
+    clearTimeout(myTimeout);
+
+  
+}
+
+function myGreeting2()
+     {
+    document.getElementById("next").style.visibility="visible";
+    
+    myStopFunction1();
+  }
+  
+  function myStopFunction1() {
+    clearTimeout(myTimeout);
+
+  
+}
